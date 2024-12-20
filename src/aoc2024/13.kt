26d9becaa772cee7710a7b0939ae9d87f96b159c
lukestead919 <lgs1997@hotmail.co.uk>
2024-12-20 10:07:31 +0000

    13 pt2

--- src/aoc2024/13.kt
+++ src/aoc2024/13.kt
@@ -4,20 +4,35 @@ import utils.Point
 import utils.println
 import utils.split
 
-private data class Machine(val target: Point, val aVector: Point, val bVector: Point) {
-    fun trySolve(): Int? =
-        (0..100)
-            .mapNotNull { aPresses ->
-                val remainder = target - (aVector * aPresses)
-                val bPressesGuess = remainder.x / bVector.x
+private data class Machine(
+    val xTarget: Long,
+    val yTarget: Long,
+    val aVector: Point,
+    val bVector: Point,
+) {
+    fun trySolve(): Long {
+        // aPresses * aVector.x + bPresses * bVector.x = target.x
+        // aPresses * aVector.y + bPresses * bVector.y = target.y
 
-                if (remainder == bVector * bPressesGuess) {
-                    (3 * aPresses) + bPressesGuess
-                } else {
-                    null
-                }
-            }
-            .minOrNull()
+        // ( a.x  b.x ) ( aPresses ) = ( target.x )
+        // ( a.y  b.y ) ( bPresses ) = ( target.y )
+
+        // ( aPresses ) = 1/                 ( b.y  -b.x ) ( target.x )
+        // ( bPresses ) = a.x*b.y - b.x*a.y  ( -a.y  a.x ) ( target.y )
+
+        val discriminant = aVector.x * bVector.y - bVector.x * aVector.y
+        if (discriminant == 0) return 0
+
+        val a1 = bVector.y * xTarget - bVector.x * yTarget
+        val b1 = aVector.x * yTarget - aVector.y * xTarget
+
+        if (a1 % discriminant != 0L || b1 % discriminant != 0L) return 0
+
+        val aPresses = a1 / discriminant
+        val bPresses = b1 / discriminant
+
+        return 3 * aPresses + bPresses
+    }
 }
 
 fun main() {
@@ -26,21 +41,34 @@ fun main() {
             .split { it == "" }
             .map { (aString, bString, targetString) ->
                 val regex = Regex(""".*?(\d+), .*?(\d+).*""")
-                fun parsePoint(s: String): Point =
-                    regex.find(s)!!.destructured.let { (x, y) -> Point(x.toInt(), y.toInt()) }
+                fun parsePair(s: String): Pair<Int, Int> =
+                    regex.find(s)!!.destructured.let { (x, y) -> x.toInt() to y.toInt() }
+
+                fun parsePoint(s: String): Point = parsePair(s).let { (x, y) -> Point(x, y) }
 
                 Machine(
                     aVector = parsePoint(aString),
                     bVector = parsePoint(bString),
-                    target = parsePoint(targetString),
+                    xTarget = parsePair(targetString).first.toLong(),
+                    yTarget = parsePair(targetString).second.toLong(),
                 )
             }
 
     fun part1() {
-        machines.mapNotNull { it.trySolve() }.sum().println()
+        machines.sumOf { it.trySolve() }.println()
     }
 
-    fun part2() {}
+    fun part2() {
+        machines
+            .map {
+                it.copy(
+                    xTarget = it.xTarget + 10000000000000,
+                    yTarget = it.yTarget + 10000000000000,
+                )
+            }
+            .sumOf { it.trySolve() }
+            .println()
+    }
 
     print("Part 1: ")
     part1()
