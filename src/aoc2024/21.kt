aafc67310f252d58ceca433bc4debb433664a021
lukestead919 <lgs1997@hotmail.co.uk>
2024-12-21 14:07:13 +0000

    Day 21 pt1 Need a completely different approach for pt 2

---
+++ src/aoc2024/21.kt
@@ -0,0 +1,197 @@
+package aoc2024
+
+import aoc2024.DirectionPadCode.*
+import utils.Direction
+import utils.Grid
+import utils.Point
+import utils.println
+
+private enum class DirectionPadCode {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    ENTER;
+
+    fun asChar(): Char {
+        return when (this) {
+            UP -> '^'
+            DOWN -> 'v'
+            LEFT -> '<'
+            RIGHT -> '>'
+            ENTER -> 'A'
+        }
+    }
+}
+
+private fun Direction.toPadCode(): DirectionPadCode {
+    return when (this) {
+        Direction.UP -> UP
+        Direction.DOWN -> DOWN
+        Direction.LEFT -> LEFT
+        Direction.RIGHT -> RIGHT
+        else -> {
+            error("Invalid direction: $this")
+        }
+    }
+}
+
+fun main() {
+    val codes = read2024Input("21")
+
+    val numpad =
+        Grid(3, 4) {
+            when (it) {
+                Point(0, 0) -> '7'
+                Point(1, 0) -> '8'
+                Point(2, 0) -> '9'
+                Point(0, 1) -> '4'
+                Point(1, 1) -> '5'
+                Point(2, 1) -> '6'
+                Point(0, 2) -> '1'
+                Point(1, 2) -> '2'
+                Point(2, 2) -> '3'
+                Point(0, 3) -> '!'
+                Point(1, 3) -> '0'
+                Point(2, 3) -> 'A'
+                else -> error("Invalid point $it")
+            }
+        }
+
+    val directionPad =
+        Grid(3, 2) {
+            when (it) {
+                Point(0, 0) -> '!'
+                Point(1, 0) -> '^'
+                Point(2, 0) -> 'A'
+                Point(0, 1) -> '<'
+                Point(1, 1) -> 'v'
+                Point(2, 1) -> '>'
+                else -> error("Invalid point $it")
+            }
+        }
+    val numpadReverse = numpad.entries.associate { (k, v) -> v to k }
+    val directionPadReverse = directionPad.entries.associate { (k, v) -> v to k }
+
+    val movesCache: MutableMap<Triple<Point, Point, Set<Point>>, List<List<Direction>>> =
+        mutableMapOf()
+
+    fun generateMoves(
+        start: Point,
+        end: Point,
+        impassablePoints: Set<Point>,
+    ): List<List<Direction>> =
+        movesCache.getOrPut(Triple(start, end, impassablePoints)) {
+            if (start == end) return listOf(emptyList())
+            if (start in impassablePoints) return emptyList()
+
+            fun movesFrom(next: Point): List<List<Direction>> {
+                return generateMoves(next, end, impassablePoints)
+            }
+
+            val moves = mutableListOf<List<Direction>>()
+            if (start.x < end.x) {
+                val moveRight = List(end.x - start.x) { Direction.RIGHT }
+                val next = start.copy(x = end.x)
+                val nextMoves = movesFrom(next).map { moveRight + it }
+                moves.addAll(nextMoves)
+            }
+            if (start.x > end.x) {
+                val moveLeft = List(start.x - end.x) { Direction.LEFT }
+                val next = start.copy(x = end.x)
+                val nextMoves = movesFrom(next).map { moveLeft + it }
+                moves.addAll(nextMoves)
+            }
+            if (start.y < end.y) {
+                val moveDown = List(end.y - start.y) { Direction.DOWN }
+                val next = start.copy(y = end.y)
+                val nextMoves = movesFrom(next).map { moveDown + it }
+                moves.addAll(nextMoves)
+            }
+            if (start.y > end.y) {
+                val moveUp = List(start.y - end.y) { Direction.UP }
+                val next = start.copy(y = end.y)
+                val nextMoves = movesFrom(next).map { moveUp + it }
+                moves.addAll(nextMoves)
+            }
+
+            //            println("Start: $start, End: $end, Impassable: $impassablePoints, Moves:
+            // $moves\n")
+
+            return moves
+        }
+
+    val numpadMovesCache: MutableMap<Pair<Point, Point>, List<List<DirectionPadCode>>> =
+        mutableMapOf()
+    fun generateMovesForNumpad(start: Point, end: Point): List<List<DirectionPadCode>> =
+        numpadMovesCache.getOrPut(start to end) {
+            generateMoves(
+                    start,
+                    end,
+                    numpad.entries.filter { it.value == '!' }.map { it.key }.toSet(),
+                )
+                .map { moves -> moves.map { it.toPadCode() } + ENTER }
+        }
+
+    val directionMovesCache: MutableMap<Pair<Point, Point>, List<List<DirectionPadCode>>> =
+        mutableMapOf()
+    fun generateMovesForDirectionPad(start: Point, end: Point): List<List<DirectionPadCode>> =
+        directionMovesCache.getOrPut(start to end) {
+            generateMoves(
+                    start,
+                    end,
+                    directionPad.entries.filter { it.value == '!' }.map { it.key }.toSet(),
+                )
+                .map { moves -> moves.map { it.toPadCode() } + ENTER }
+        }
+
+    fun part1() {
+        fun calculateDirectionsForCode(code: String): List<List<DirectionPadCode>> {
+            val isNumberCode = code.contains(Regex("[0-9]"))
+            val pad = if (isNumberCode) numpadReverse else directionPadReverse
+            val generateMovesForPad =
+                if (isNumberCode) ::generateMovesForNumpad else ::generateMovesForDirectionPad
+
+            return ("A$code").zipWithNext().fold(listOf(emptyList())) {
+                previousMoves,
+                (startValue, endValue) ->
+                val start = pad.getValue(startValue)
+                val end = pad.getValue(endValue)
+                val validMoves = generateMovesForPad(start, end)
+
+                previousMoves.flatMap { previousMove -> validMoves.map { previousMove + it } }
+            }
+        }
+
+        codes
+            .map { startCode ->
+                (0..3).fold(listOf(listOf(startCode))) { previousCodes, _ ->
+                    previousCodes
+                        .flatMap { code ->
+                            //                        code.println()
+                            calculateDirectionsForCode(code.last()).map {
+                                code + it.map { it.asChar() }.joinToString(separator = "")
+                            }
+                        }
+                        .filter { code ->
+                            val shortestLengthSoFar = code.minOf { code.last().length }
+                            code.last().length <= shortestLengthSoFar + 3
+                        }
+                }
+            }
+            .map {
+                val startingCode = it[0][0]
+                val startingCodeValue = startingCode.dropLast(1).toInt()
+                it.minOf { it.last().length } * startingCodeValue
+            }
+            .sum()
+            .println()
+    }
+
+    fun part2() {}
+
+    print("Part 1: ")
+    part1()
+    print("Part 2: ")
+    part2()
+}
