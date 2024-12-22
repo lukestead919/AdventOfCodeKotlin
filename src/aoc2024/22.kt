ca867232a03bc01bc103fa2bf87aff1f812f5b85
lukestead919 <lgs1997@hotmail.co.uk>
2024-12-22 13:41:44 +0000

    Day 22 - Easier than yesterday :joy:

---
+++ src/aoc2024/22.kt
@@ -0,0 +1,50 @@
+package aoc2024
+
+import utils.combinations
+import utils.iterate
+import utils.println
+
+fun Long.mix(other: Long): Long = this xor other
+
+fun Long.prune(): Long = Math.floorMod(this, 16777216L)
+
+fun Long.nextSecret(): Long =
+    let { mix(64 * this).prune() }.let { it.mix(it / 32).prune() }.let { it.mix(it * 2048).prune() }
+
+fun main() {
+    val secrets = read2024Input("22").map { it.toLong() }
+
+    fun part1() {
+        secrets.sumOf { it.iterate(2000) { it.nextSecret() } }.println()
+    }
+
+    fun part2() {
+        val secretMaps =
+            secrets.map { startingSecret ->
+                (1..2000)
+                    .asSequence()
+                    .runningFold(startingSecret) { previousSecret, _ ->
+                        previousSecret.nextSecret()
+                    }
+                    .map { (it % 10).toInt() }
+                    .zipWithNext()
+                    .map { (a, b) -> b - a to b }
+                    .windowed(4)
+                    .toList()
+                    .reversed()
+                    .associate { window -> window.map { it.first } to window.last().second }
+            }
+
+        val sequences =
+            (-9..9).toList().combinations(3).flatMap { beginning ->
+                (1..9).map { end -> beginning + end }
+            }
+
+        sequences.maxOf { sequence -> secretMaps.sumOf { it.getOrDefault(sequence, 0) } }.println()
+    }
+
+    print("Part 1: ")
+    part1()
+    print("Part 2: ")
+    part2()
+}
